9381f5c2ece6320fed3ca5c5a8eb5c69ba07f78b
yangshun <yangshun@richinfo.cn>
2024-01-12 17:45:04 +0800

    自定义代码修改

--- process.py
+++ process.py
@@ -263,7 +263,7 @@ def reservation(params: dict, mobile: str):
     #     raise RuntimeError
 
     msg = f'预约:{mobile};Code:{responses.status_code};Body:{responses.text};'
-    logging.info(msg)
+    logging.info(f'预约:{mobile[:5]};Code:{responses.status_code};Body:{responses.status_code};')
 
     # 如果是成功，推送消息简化；失败消息则全量推送
     if responses.status_code == 200:
@@ -335,4 +335,4 @@ def getUserEnergyAward(mobile: str):
                              headers=headers, json={})
     # response.json().get('message') if '无法领取奖励' in response.text else "领取奖励成功"
     logging.info(
-        f'领取耐力 : mobile:{mobile} :  response code : {response.status_code}, response body : {response.text}')
+        f'领取耐力 : mobile:{mobile[:5]} :  response code : {response.status_code}, response body : {response.text}')
